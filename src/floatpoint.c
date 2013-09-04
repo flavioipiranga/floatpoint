3842d12e6262da080c5c5e4fc4565a95a7e9cfc7
flaviojr <flaviojr@users.noreply.github.com>
2013-09-03 19:20:37 -0700

    Create floatpoint.c

---
+++ src/floatpoint.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BIT0  "0"
+#define BIT1  "1"
+
+//Real para binário
+
+char* InvString(char* string){
+    int tam =0, j, i;
+    char* aux;
+ 
+    // obtendo o tamanho da string
+    while(string[tam] != '\0')
+        tam++;
+ 
+    // alocando espaco
+    aux = (char*)malloc(tam * sizeof(char));
+    for(i = tam - 1, j = 0; i >= 0; i--, j++)
+        aux[j] = string[i];
+    aux[j] = '\0';
+    
+    return aux;
+    
+}
+
+char* InttoBin(char* in){
+	char bin[32];
+	int deci;
+	
+	int i;
+
+    if(deci == 0)
+        strcat(bin, BIT0);
+
+    for(i = 0; i<32; i++){
+        if(deci == 0)
+            break;
+			         
+        if(deci%2)
+            strcat(bin, BIT1);
+            
+        else strcat(bin, BIT0);
+            
+        deci = deci/2;
+        
+         
+    }
+    
+    strcpy(bin, InvString(bin));
+
+	
+	return bin;
+	}
+
+
+char* FloattoBin (char* in) {
+	
+	
+	
+	
+	
+	return in;
+	}
+
+char* RealtoFloatPoint(char* in){
+	
+	char **endptr;
+	double num;
+	puts(in);
+	num = strtod(in, endptr);
+	
+	printf("Teste:%lf", num);
+	
+	return in;	
+	
+	}
