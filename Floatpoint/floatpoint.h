00081fe0504c20c4a7d54e6eac14a5c96c102d58
Flavio Ipiranga <flavio.ipiranga+progamando@gmail.com>
2013-09-04 22:15:56 -0400

    Primeira execução com Makefile

---
+++ Floatpoint/floatpoint.h
@@ -0,0 +1,4 @@
+char* InvString(char* string);
+char* InttoBin(char* in);
+char* FloattoBin (char* in);
+char* RealtoFloatPoint(char* in);
---
+++ Floatpoint/main.c
@@ -0,0 +1,26 @@
+/*
+ * main.c
+ *
+ *  Created on: 04/09/2013
+ *      Author: flavioipiranga
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+#include "floatpoint.h"
+/*
+ *
+ */
+int main(int argc, char** argv) {
+
+
+printf("hello world");
+
+
+
+    return (EXIT_SUCCESS);
+}
+
+
