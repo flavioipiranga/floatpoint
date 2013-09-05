e03afbb2a33066bf160be5063d1039839b447788
Flavio Ipiranga <flavio.ipiranga@gmail.com>
2013-09-05 09:16:57 -0400

    Update floatpoint.c

--- Floatpoint/floatpoint.c
+++ Floatpoint/floatpoint.c
@@ -1,3 +1,10 @@
+/*
+ * floatpoint.c
+ *
+ *  Created on: 05/09/2013
+ *      Author: flavioipiranga
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -5,32 +12,31 @@
 #define BIT0  "0"
 #define BIT1  "1"
 
-//Real para binário
+//Real para binario
+
 
 char* InvString(char* string){
     int tam =0, j, i;
     char* aux;
-//sadas
- 	//asdsa
- 	
+
     // obtendo o tamanho da string
     while(string[tam] != '\0')
         tam++;
- 
+
     // alocando espaco
     aux = (char*)malloc(tam * sizeof(char));
     for(i = tam - 1, j = 0; i >= 0; i--, j++)
         aux[j] = string[i];
     aux[j] = '\0';
-    
+
     return aux;
-    
+
 }
 
 char* InttoBin(char* in){
 	char bin[32];
 	int deci;
-	
+
 	int i;
 
     if(deci == 0)
@@ -39,42 +45,42 @@ char* InttoBin(char* in){
     for(i = 0; i<32; i++){
         if(deci == 0)
             break;
-			         
+
         if(deci%2)
             strcat(bin, BIT1);
-            
+
         else strcat(bin, BIT0);
-            
+
         deci = deci/2;
-        
-         
+
+
     }
-    
+
     strcpy(bin, InvString(bin));
 
-	
+
 	return bin;
 	}
 
 
 char* FloattoBin (char* in) {
-	
-	
-	
-	
-	
+
+
+
+
+
 	return in;
 	}
 
 char* RealtoFloatPoint(char* in){
-	
+
 	char **endptr;
 	double num;
 	puts(in);
 	num = strtod(in, endptr);
-	
+
 	printf("Teste:%lf", num);
-	
-	return in;	
-	
+
+	return in;
+
 	}
